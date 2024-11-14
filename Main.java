923d5f0ec267014ae5483cffda22ae48e4fe9939
George <lunohod0002@gmail.com>
2024-11-14 14:32:12 +0300

    Add summation in Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
-
+        int a=10;
+        int b=5;
+        System.out.println(a+b);
     }
 }
