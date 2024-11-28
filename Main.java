444cb70abec9626382949fcba46688fa7c8702b9
George <lunohod0002@gmail.com>
2024-11-28 14:09:48 +0300

    Add for loop from 10 to 20 in Main.java

--- Main.java
+++ Main.java
@@ -3,5 +3,9 @@ public class Main {
         for (int i=0;i<10;i++){
             System.out.println(i);
         }
+        for (int j=10; j<10;j++){
+            System.out.println(j);
+        }
+
     }
 }
