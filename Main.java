d49201d6261a35002944d7da8f1f8000ff2af059
George <lunohod0002@gmail.com>
2024-11-28 14:07:38 +0300

    Add for loop in Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        for (int i=0;i<10;i++){
+            System.out.println(i);
+        }
         System.out.print("Это добавлено в ветке main");
         int a=10;
         int b=5;
