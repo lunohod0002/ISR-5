76c02c4a4c7bf6123ae70f8dbd5f971218adf83b
George <lunohod0002@gmail.com>
2024-11-28 14:23:38 +0300

    Add console input

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         for (int i=0;i<10;i++){
             System.out.println(i);
         }
-        for (int j=10; j<10;j++){
+        for (int j=10; j<20;j++){
             System.out.println(j);
         }
         for (int q=20;q<30;q++){
@@ -12,6 +14,11 @@ public class Main {
         for (int w=30;w<40;w++){
             System.out.println(w);
         }
+        Scanner scanner=new Scanner(System.in);
+        int a=Integer.parseInt(scanner.nextLine());
+        int b=Integer.parseInt(scanner.nextLine());
+        System.out.println(a+b);
+
 
     }
 }
